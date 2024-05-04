b2ed6a36e512356346cbdf9a174e57baf45fffd1
https://github.com/Sanjeevkumar-woks <sanjeevmanagutti@gmail.com>
2024-05-04 20:46:30 +0530

    solved new one

---
+++ palindromeNumber.js
@@ -0,0 +1,39 @@
+// Given an integer x, return true if x is a
+// palindrome
+// , and false otherwise.
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+const x = 121;
+
+var isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+  let reverse = 0;
+  for (let num = x; num; num = Math.trunc(num / 10)) {
+    let digit = num % 10;
+    reverse = reverse * 10 + digit;
+  }
+  if (reverse == x) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isPalindrome(x));
